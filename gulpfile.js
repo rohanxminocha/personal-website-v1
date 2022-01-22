e254d6ff1a732b4566a9d382dc7b73656620f123
Rohan Minocha <rohan.minocha2002@gmail.com>
2022-01-22 00:24:07 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
-const gulp = require('gulp');
-const sass = require('./build/sass');
+'use strict';
+
+const gulp    = require('gulp');
+const sass    = require('./build/sass');
 const scripts = require('./build/scripts');
-const images = require('./build/images');
-const sync = require('./build/browsersync');
+const images  = require('./build/images');
+const sync    = require('./build/browsersync');
 
 [sass, scripts, images, sync].forEach(task => {
   task(gulp);
 });
 
-gulp.task('jekyll', function (){
-    spawn('jekyll.bat', ['build'], {stdio: 'inherit'});
-});
-
-gulp.task('build', gulp.series(['sass', 'scripts', 'images', 'jekyll-build']));
+gulp.task('build', ['sass', 'scripts', 'images', 'jekyll-build']);
